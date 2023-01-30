7919960f76017140b2eda7832092251954925c2f
MikkySnow <mike@swisstronik.com>
2023-01-30 18:25:58 +0100

    little refactor

--- x/evm/keeper/sgxvm_connector_test.go
+++ x/evm/keeper/sgxvm_connector_test.go
@@ -8,6 +8,40 @@ import (
 	"math/big"
 )
 
+func insertAccount(
+	connector *evmkeeper.Connector,
+	address common.Address,
+	balance, nonce *big.Int,
+) error {
+	// Encode request
+	request, encodeErr := proto.Marshal(&librustgo.CosmosRequest{
+		Req: &librustgo.CosmosRequest_InsertAccount{
+			InsertAccount: &librustgo.QueryInsertAccount{
+				Address: address.Bytes(),
+				Balance: balance.Bytes(),
+				Nonce:   nonce.Bytes(),
+			},
+		},
+	})
+
+	if encodeErr != nil {
+		return encodeErr
+	}
+
+	responseBytes, queryErr := connector.Query(request)
+	if queryErr != nil {
+		return queryErr
+	}
+
+	response := &librustgo.QueryInsertAccountResponse{}
+	decodingError := proto.Unmarshal(responseBytes, response)
+	if decodingError != nil {
+		return decodingError
+	}
+
+	return nil
+}
+
 func (suite *KeeperTestSuite) TestSGXVMConnector() {
 	var (
 		connector evmkeeper.Connector
@@ -25,30 +59,12 @@ func (suite *KeeperTestSuite) TestSGXVMConnector() {
 		{
 			"Should be able to insert account",
 			func() {
-				// Create request
 				addressToSet := common.BigToAddress(big.NewInt(100))
 				balanceToSet := big.NewInt(10000)
 				nonceToSet := big.NewInt(1)
 
-				// Encode request
-				request, encodeErr := proto.Marshal(&librustgo.CosmosRequest{
-					Req: &librustgo.CosmosRequest_InsertAccount{
-						InsertAccount: &librustgo.QueryInsertAccount{
-							Address: addressToSet.Bytes(),
-							Balance: balanceToSet.Bytes(),
-							Nonce:   nonceToSet.Bytes(),
-						},
-					},
-				})
-				suite.Require().NoError(encodeErr)
-
-				// Send request to the connector
-				responseBytes, queryErr := connector.Query(request)
-				suite.Require().NoError(queryErr)
-
-				response := &librustgo.QueryInsertAccountResponse{}
-				decodingError := proto.Unmarshal(responseBytes, response)
-				suite.Require().NoError(decodingError)
+				err := insertAccount(&connector, addressToSet, balanceToSet, nonceToSet)
+				suite.Require().NoError(err)
 
 				// Check if account was inserted correctly
 				balance := connector.Keeper.GetBalance(connector.Ctx, addressToSet)
