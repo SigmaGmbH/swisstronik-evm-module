acd28c6b1999eccb9f0a8ce73ffa78237e322576
MikkySnow <mike@swisstronik.com>
2023-04-12 15:39:12 +0000

    add draft of commands

---
+++ x/attestation/module.go
@@ -0,0 +1,68 @@
+package attestation
+
+import (
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/SigmaGmbH/evm-module/x/attestation/client/cli"
+	"github.com/SigmaGmbH/evm-module/x/attestation/types"
+)
+
+var (
+	_ module.AppModuleBasic = AppModuleBasic{}
+)
+
+// AppModuleBasic defines the basic application module used by the fee market module.
+type AppModuleBasic struct{}
+
+// Name returns the fee market module's name.
+func (AppModuleBasic) Name() string {
+	return types.ModuleName
+}
+
+// RegisterLegacyAminoCodec performs a no-op
+func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {}
+
+// ConsensusVersion returns the consensus state-breaking version for the module.
+func (AppModuleBasic) ConsensusVersion() uint64 {
+	return 4
+}
+
+// DefaultGenesis returns default genesis state as raw bytes for the fee market
+// module.
+func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	return nil
+}
+
+// ValidateGenesis is the validation check of the Genesis
+func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, _ client.TxEncodingConfig, bz json.RawMessage) error {
+	return nil
+}
+
+// RegisterRESTRoutes performs a no-op as the EVM module doesn't expose REST
+// endpoints
+func (AppModuleBasic) RegisterRESTRoutes(_ client.Context, _ *mux.Router) {
+}
+
+func (b AppModuleBasic) RegisterGRPCGatewayRoutes(c client.Context, serveMux *runtime.ServeMux) {}
+
+// GetTxCmd returns the root tx command for the fee market module.
+func (AppModuleBasic) GetTxCmd() *cobra.Command {
+	return nil
+}
+
+// GetQueryCmd returns no root query command for the fee market module.
+func (AppModuleBasic) GetQueryCmd() *cobra.Command {
+	return cli.GetQueryCmd()
+}
+
+// RegisterInterfaces registers interfaces and implementations of the fee market module.
+func (AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {}
